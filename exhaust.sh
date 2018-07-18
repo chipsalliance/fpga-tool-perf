af9d732748faf20e21c147afdd6397fd4e9b2e51
John McMaster <johndmcmaster@gmail.com>
2018-07-17 20:23:16 -0700

    exhaust: --toolchain, --project
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- exhaust.sh
+++ exhaust.sh
@@ -2,12 +2,20 @@
 
 device="hx8k"
 package="ct256"
+pcf=
+dry=
+aproject=
+atoolchain=
 
 usage() {
     echo "Exhaustively try project-toolchain combinations, seeding if possible"
     echo "usage: exaust.sh [args]"
-    echo "--device         device (default: hx8k)"
-    echo "--package        device package (default: ct256)"
+    echo "--device <device>         device (default: hx8k)"
+    echo "--package <package>       device package (default: ct256)"
+    echo "--project <project>       run given project only (default: all)"
+    echo "--toolchain <toolchain>   run given toolchain only (default: all)"
+    echo "--pcf <pcf>               pin constraint file (default: none)"
+    echo "--dry                     print commands, don't invoke"
 }
 
 ARGS=()
@@ -20,6 +28,25 @@ while [[ $# -gt 0 ]]; do
         ;;
     --package)
         package=$2
+        shift
+        shift
+        ;;
+    --pcf)
+        pcf=$2
+        shift
+        shift
+        ;;
+    --dry)
+        dry=echo
+        shift
+        ;;
+    --project)
+        aproject=$2
+        shift
+        shift
+        ;;
+    --toolchain)
+        atoolchain=$2
         shift
         shift
         ;;
@@ -42,14 +69,20 @@ function run() {
     toolchain=$1
     project=$2
 
+    if [ "$pcf" = "" ] ; then
+        pcf_arg=""
+    else
+        pcf_arg="--pcf $pcf"
+    fi
+
     if [[ $(python3 main.py --list-seedable) = *"$toolchain"* ]]; then
         for seed in 1 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000 ; do
             # some of these may fail pnr
-            python3 main.py --toolchain $toolchain --project $project --device $device --package $package --seed $seed || true
+            $dry python3 main.py --toolchain $toolchain --project $project --device $device --package $package --seed $seed $pcf_arg || true
         done
     else
         # some of these may fail pnr
-        python3 main.py --toolchain $toolchain --project $project --device $device --package $package || true
+        $dry python3 main.py --toolchain $toolchain --project $project --device $device --package $package $pcf_arg || true
     fi
     # make ^C easier
     sleep 0.1
@@ -59,7 +92,13 @@ function run() {
 function exhaustive() {
     echo "Running exhaustive project-toolchain search"
     for project in $(python3 main.py --list-projects) ; do
+        if [ -n "$aproject" -a "$aproject" != "$project" ] ; then
+            continue
+        fi
         for toolchain in $(python3 main.py --list-toolchains) ; do
+            if [ -n "$atoolchain" -a "$atoolchain" != "$toolchain" ] ; then
+                continue
+            fi
             echo
             run $toolchain $project
         done
