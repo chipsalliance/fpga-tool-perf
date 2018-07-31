5b48db750ea43a2a959da42d84100b4c3f0a2b29
John McMaster <johndmcmaster@gmail.com>
2018-07-30 20:06:56 -0700

    vpr_xml.sh: only build xml, clean, require VPR
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- vpr_xml.sh
+++ vpr_xml.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -ex
 
 if [ '!' -d "$SFAD_DIR" ] ; then
@@ -5,16 +7,25 @@ if [ '!' -d "$SFAD_DIR" ] ; then
     exit 1
 fi
 
+# FIXME: as of this writing SFAD has broken vpr in conda
+# https://github.com/SymbiFlow/fpga-tool-perf/issues/10
+if [ -z "$VPR" ] ; then
+    echo "VPR invalid"
+    exit 1
+fi
+
 cd $SFAD_DIR/ice40/tests/blink || cd $SFAD_DIR/tests/ice40/blink
 
-# tim: maybe run make route instead of bit
-
 # hx1k (about 3 min on carbon x1)
-make BOARD=icestick bit
+echo "Building hx1k XML"
+time make BOARD=icestick dist-clean rr_graph.real.xml &>icestick.txt
+exit 1
 # hx8k (about 15 min on carbon x1)
-make BOARD=tinyfpga-b2 bit
+echo "Building hx8k XML"
+time make BOARD=tinyfpga-b2 dist-clean rr_graph.real.xml &>tinyfpga-b2.txt
 
 # up5k
 touch test-upk5-uwg30.pcf
-make BOARD=test-upk5-uwg30 DEVICE=up5k PACKAGE=uwg30 PROG_TOOL=/dev/null bit
+echo "Building up5k XML"
+time make BOARD=test-upk5-uwg30 DEVICE=up5k PACKAGE=uwg30 PROG_TOOL=/dev/null rr_graph.real.xml  &>test-upk5-uwg30.txt
 
