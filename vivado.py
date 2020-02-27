867b2b21170e293148a7aa7eb8c06f9c3e47d3bf
Karol Gugala <kgugala@antmicro.com>
2020-02-27 19:39:35 +0100

    vivado: Pass VIVADO define to designs
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- vivado.py
+++ vivado.py
@@ -51,13 +51,15 @@ class Vivado(Toolchain):
                 'files': self.files,
                 'name': self.project_name,
                 'toplevel': self.top,
-                'parameters' : {
-                    'VIVADO' : {
-                        'paramtype' : 'vlogdefine',
-                        'datatype' : 'int',
-                        'default' : 1,
+                'parameters':
+                    {
+                        'VIVADO':
+                            {
+                                'paramtype': 'vlogdefine',
+                                'datatype': 'int',
+                                'default': 1,
+                            },
                     },
-                },
                 'tool_options':
                     {
                         'vivado': {
