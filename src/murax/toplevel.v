4ed8f1ac400f1f81a169fcefbdf41c2af3823ee0
Alessandro Comodi <acomodi@antmicro.com>
2020-04-02 17:39:28 +0200

    murax: fix constraints file
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- src/murax/toplevel.v
+++ src/murax/toplevel.v
@@ -1,51 +1,40 @@
 `timescale 1ns / 1ps
 
 module toplevel(
-    input   clk,
-    input   jtag_clk,
-    input   io_G15,
-    output  io_G16,
-    input   io_F15,
-    output  io_B12,
-    input   io_B10,
-    output [7:0] io_led
+    input   io_mainClk,
+    output  io_uart_txd,
+    input   io_uart_rxd,
+    input [15:0] sw,
+    output [15:0] io_led
   );
+
+  wire io_mainClk_bufg;
+  BUFG bufg(.I(io_mainClk), .O(io_mainClk_bufg));
 
   wire [31:0] io_gpioA_read;
   wire [31:0] io_gpioA_write;
   wire [31:0] io_gpioA_writeEnable;
   wire io_mainClk;
   wire io_jtag_tck;
+  wire io_jtag_tdi;
+  wire io_jtag_tdo;
+  wire io_jtag_tms;
+  wire io_uart_txd;
+  wire io_uart_rxd;
 
-  assign io_mainClk = clk;
-  /*
-  SB_GB mainClkBuffer (
-    .USER_SIGNAL_TO_GLOBAL_BUFFER (clk),
-    .GLOBAL_BUFFER_OUTPUT ( io_mainClk)
-  );
-  */
-
-  assign io_jtag_tck = jtag_clk;
-  /*
-  SB_GB jtagClkBuffer (
-    .USER_SIGNAL_TO_GLOBAL_BUFFER (jtag_clk),
-    .GLOBAL_BUFFER_OUTPUT ( io_jtag_tck)
-  );
-  */
-
-  assign io_led = io_gpioA_write[7 : 0];
+  assign io_led = io_gpioA_write[15: 0];
+  assign io_gpioA_read[15:0] = sw;
 
   Murax murax (
     .io_asyncReset(0),
-    .io_mainClk (io_mainClk ),
-    .io_jtag_tck(io_jtag_tck),
-    .io_jtag_tdi(io_G15),
-    .io_jtag_tdo(io_G16),
-    .io_jtag_tms(io_F15),
+    .io_mainClk (io_mainClk_bufg ),
+    .io_jtag_tck(1'b0),
+    .io_jtag_tdi(1'b0),
+    .io_jtag_tms(1'b0),
     .io_gpioA_read       (io_gpioA_read),
     .io_gpioA_write      (io_gpioA_write),
     .io_gpioA_writeEnable(io_gpioA_writeEnable),
-    .io_uart_txd(io_B12),
-    .io_uart_rxd(io_B10)
+    .io_uart_txd(io_uart_txd),
+    .io_uart_rxd(io_uart_rxd)
   );
 endmodule
--- src/murax/xc7/murax.sdc
+++
@@ -1,1 +0,0 @@
-create_clock -name clk -period 20
