2c9aa4d647c89833a4c071f4068a5aabc83214da
Tomasz Jurtsch <tjurtsch@antmicro.com>
2020-04-03 08:57:05 +0200

    Remove unused scripts
    Signed-off-by: Tomasz Jurtsch <tjurtsch@antmicro.com>

--- csv.sh
+++
@@ -1,7 +0,0 @@
-# header at top
-cat $(find . -mindepth 1 -name '*.csv') |sort -u > all.csv.tmp
-cat all.csv.tmp |grep "Build,Date" >all.csv
-cat all.csv.tmp |grep -v "Build,Date" >>all.csv
-
-python ../sow.py all.csv sow.csv
-
--- json.sh
+++
@@ -1,4 +0,0 @@
-# header at top
-echo $(find . -mindepth 1 -name 'meta.json') > all.json
-python ../sow.py all.json sow.json
-
