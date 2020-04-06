e395df7e32518c5bb027f3c95b942cfd27343817
Alessandro Comodi <acomodi@antmicro.com>
2020-04-06 23:34:37 +0200

    update environmental settings
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- env.sh
+++ env.sh
@@ -11,3 +11,4 @@ source ${VIVADO_SETTINGS}
 
 . ./env/bin/activate
 source utils/environment.python.sh
+source settings.sh
--- utils/environment.python.sh
+++ utils/environment.python.sh
@@ -1,6 +1,6 @@
 # FIXME: fasm should be installed into the running Python environment.
 BASE_DIR=${FPGA_TOOL_PERF_DIR}
-export PYTHONPATH="$BASE_DIR:$BASE_DIR/third_party/fasm:$BASE_DIR/third_party/edalize:$PYTHONPATH"
+export PYTHONPATH="${SYMBIFLOW}/python:$BASE_DIR:$BASE_DIR/third_party/fasm:$BASE_DIR/third_party/edalize:$PYTHONPATH"
 
 # Suppress the following warnings;
 # - env/lib/python3.7/distutils/__init__.py:4: DeprecationWarning: the imp module is deprecated in favour of importlib; see the module's documentation for alternative uses
