db3090e9fa544de22659d425e4e8161c3cdc5c81
Alessandro Comodi <acomodi@antmicro.com>
2020-04-22 17:49:50 +0200

    conda: add fasm and edalize in requirements.txt
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- utils/environment.python.sh
+++ utils/environment.python.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR=${FPGA_TOOL_PERF_BASE_DIR}
 # FIXME: fasm should be installed into the running Python environment.
-export PYTHONPATH="${SYMBIFLOW}/bin/python:$BASE_DIR:$BASE_DIR/third_party/fasm:$BASE_DIR/third_party/edalize:$PYTHONPATH"
+export PYTHONPATH="${SYMBIFLOW}/bin/python:$PYTHONPATH"
 
 # Suppress the following warnings;
 # - env/lib/python3.7/distutils/__init__.py:4: DeprecationWarning: the imp module is deprecated in favour of importlib; see the module's documentation for alternative uses
