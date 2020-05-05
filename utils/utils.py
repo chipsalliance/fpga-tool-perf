13075317f7beae3ab7768d861ef668efcc49b3f7
Alessandro Comodi <acomodi@antmicro.com>
2020-05-05 10:39:36 +0200

    dataframe: use helper class to query dataframe
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- utils/utils.py
+++ utils/utils.py
@@ -1,66 +1,91 @@
-def get_columns(df):
-  return ", ".join(df.columns)
+class DataFrame(object):
+    """Holds helper functions to query the data frame containing
+    the results of a run."""
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
 
-def get_designs_list(df):
-  return sorted(set(df.loc[:,'design']))
+    def get_columns(self):
+        return self.dataframe.columns
 
-def get_project_data(df, column, callback, project=None):
-  project_df = df.set_index('project')
+    def get_designs_list(self):
+        return sorted(set(self.dataframe.loc[:, 'project']))
 
-  projects = [project] if project else get_project_list(df)
+    def get_project_data(self, column, callback, project=None):
+        project_df = self.dataframe.set_index('project')
 
-  data = dict()
-  for prj in projects:
-    data[prj] = dict()
-    prj_data = project_df.loc[prj, ['toolchain', 'board', column]]
+        projects = [project] if project else get_project_list(self.dataframe)
 
-    for idx, row_data in prj_data.iterrows():
-      board = row_data['board']
-      if board not in data[prj].keys():
-        data[prj][board] = []
+        data = dict()
 
-      element = callback(row_data[column])
-      element['toolchain'] = row_data['toolchain']
+        for prj in projects:
+            data[prj] = dict()
+            prj_data = project_df.loc[prj, ['toolchain', 'board', column]]
 
-      data[prj][board].append(element)
+            for idx, row_data in prj_data.iterrows():
+                board = row_data['board']
+                if board not in data[prj].keys():
+                    data[prj][board] = []
 
-  return data
+                element = callback(row_data[column])
+                element['toolchain'] = row_data['toolchain']
 
-def runtime_callback(data):
-  element = dict()
-  element['runtime'] = dict()
+                data[prj][board].append(element)
 
-  for k, v in data.items():
-    if type(v) == type(dict()):
-      for k1, v1 in v.items():
-        element['runtime'][k1] = ("%0.3f" % v1)
-    else:
-      element['runtime'][k] = ("%0.3f" % v)
+        return data
 
-  return element
+    def get_runtime(self, project=None):
+        def runtime_callback(data):
+            element = dict()
+            element['runtime'] = dict()
 
-def resource_utilization_callback(data):
-  element = dict()
-  element['resources'] = dict()
+            for k, v in data.items():
+                if type(v) == type(dict()):
+                    for k1, v1 in v.items():
+                        element['runtime'][k1] = ("%0.3f" % v1)
+                else:
+                    element['runtime'][k] = ("%0.3f" % v)
 
-  for k, v in data.items():
-    element['resources'][k] = v
+            return element
 
-  return element
+        return self.get_project_data('runtime', runtime_callback, project)
 
+    def get_resources(self, project=None):
+        def resource_utilization_callback(data):
+            element = dict()
+            element['resources'] = dict()
 
-def max_freq_callback(data):
-  element = dict()
-  element['max_freq'] = dict()
+            for k, v in data.items():
+                element['resources'][k] = v
 
-  if type(data) is float:
-    element['max_freq'] = ("%0.3f MHz" % (data / 1e6))
-  elif type(data) is dict:
-    for cd in data:
-      element['max_freq']['actual'] = "%0.3f MHz" % (data[cd]['actual'] / 1e6)
-      element['max_freq']['requested'] = "%0.3f MHz" % (data[cd]['requested'] / 1e6)
-      element['max_freq']['met'] = data[cd]['met']
-      element['max_freq']['s_violation'] = ("%0.3f ns" % data[cd]['setup_violation'])
-      element['max_freq']['h_violation'] = ("%0.3f ns" % data[cd]['hold_violation'])
+            return element
 
-  return element
+        return self.get_project_data(
+            'resources', resource_utilization_callback, project
+        )
+
+    def get_max_freq(self, project=None):
+        def max_freq_callback(data):
+            element = dict()
+            element['max_freq'] = dict()
+
+            if type(data) is float:
+                element['max_freq'] = ("%0.3f MHz" % (data / 1e6))
+            elif type(data) is dict:
+                for cd in data:
+                    element['max_freq']['actual'] = "%0.3f MHz" % (
+                        data[cd]['actual'] / 1e6
+                    )
+                    element['max_freq']['requested'] = "%0.3f MHz" % (
+                        data[cd]['requested'] / 1e6
+                    )
+                    element['max_freq']['met'] = data[cd]['met']
+                    element['max_freq']['s_violation'] = (
+                        "%0.3f ns" % data[cd]['setup_violation']
+                    )
+                    element['max_freq']['h_violation'] = (
+                        "%0.3f ns" % data[cd]['hold_violation']
+                    )
+
+            return element
+
+        return self.get_project_data('max_freq', max_freq_callback, project)
