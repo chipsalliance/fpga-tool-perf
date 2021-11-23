e605170fcd8ee85964ce8a4a61d0bfd55d907835
Karol Gugala <kgugala@antmicro.com>
2021-11-23 10:22:29 +0100

    split matrixes
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- .github/workflows/scripts/generate_job_matrix.py
+++ .github/workflows/scripts/generate_job_matrix.py
@@ -10,16 +10,28 @@
 # SPDX-License-Identifier: ISC
 
 from fpgaperf import get_combinations
+import sys
 
-jobs = list()
+if len(sys.argv) < 2:
+    print("Usage {} <tool>".format(sys.argv[0]))
+    sys.exit(1)
+
+tools = sys.arg[1:]
+
+jobs = dict()
 for combination in get_combinations():
     project, toolchain, board = combination
 
     # TODO: different matrices need to be produced, otherwise the 256 jobs limit is reached
     #       for now test only vivado and vpr tests
-    if toolchain not in ["vpr"]:
+    if toolchain not in tools:
         continue
 
-    jobs.append(dict(project=project, toolchain=toolchain, board=board))
+    if toolchain not in tools:
+        jobs[toolchain] = list()
 
-print('::set-output name=matrix::' + str(jobs))
+    jobs[toolchain].append(dict(project=project, toolchain=toolchain, board=board))
+
+for tool in tools:
+    if tool in jobs:
+        print('::set-output name=matrix_{}::'.format(tool) + str(jobs[tool]))
