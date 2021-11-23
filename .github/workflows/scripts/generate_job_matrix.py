70bb6b8a0744374833b5d6e258566a3f774151d3
Alessandro Comodi <acomodi@antmicro.com>
2021-11-23 12:56:54 +0100

    add remaining tests and install only necessary packages
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/workflows/scripts/generate_job_matrix.py
+++ .github/workflows/scripts/generate_job_matrix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020  The SymbiFlow Authors.
+# Copyright (C) 2021  The SymbiFlow Authors.
 #
 # Use of this source code is governed by a ISC-style
 # license that can be found in the LICENSE file or at
@@ -18,13 +18,15 @@ if len(sys.argv) < 2:
 
 tools = sys.argv[1:]
 
+all_toolchains = "all" in tools
+
 jobs = dict()
 for combination in get_combinations():
     project, toolchain, board = combination
 
     # TODO: different matrices need to be produced, otherwise the 256 jobs limit is reached
     #       for now test only vivado and vpr tests
-    if toolchain not in tools:
+    if toolchain not in tools and not all_toolchains:
         continue
 
     if toolchain not in jobs:
@@ -32,6 +34,5 @@ for combination in get_combinations():
 
     jobs[toolchain].append(dict(project=project, toolchain=toolchain, board=board))
 
-for tool in tools:
-    if tool in jobs:
-        print('::set-output name=matrix_{}::'.format(tool) + str(jobs[tool]))
+for tool, matrix in jobs.items():
+    print(f"::set-output name=matrix_{tool.replace('-', '_')}:: {matrix}")
