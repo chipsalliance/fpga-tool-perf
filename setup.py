10e8804becce531320bf82b5ad5808a1b1ac25f2
Karol Gugala <kgugala@antmicro.com>
2022-01-10 18:58:20 +0100

    Re-license to Apache-2.0
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021  The SymbiFlow Authors.
+# Copyright 2018-2022 F4PGA Authors
 #
-# Use of this source code is governed by a ISC-style
-# license that can be found in the LICENSE file or at
-# https://opensource.org/licenses/ISC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier:	ISC
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 import setuptools
 
@@ -24,7 +32,7 @@ setuptools.setup(
                 "fpga-tool-perf=fpgaperf:main"
             ]
     },
-    author="SymbiFlow Authors",
+    author="F4PGA Authors",
     author_email="symbiflow@lists.librecores.org",
     description="Python library to run FPGA benchmarks on various toolchains",
     long_description=long_description,
@@ -33,7 +41,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: ISC License",
+        "License :: OSI Approved :: Apache-2.0",
         "Operating System :: OS Independent",
     ],
 )
