48969d49ec005c1ae53250c2f67efe96517c9a21
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-17 00:54:41 +0200

    utils/conda: cleanup
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- utils/conda.sh
+++ utils/conda.sh
@@ -16,18 +16,17 @@
 # limitations under the License.
 #
 
+set -e
+
 export CONDA_DIR=$(pwd)/env/conda
 export PATH=$CONDA_DIR/bin:${PATH}
+
 (
     if [[ ! -e ${CONDA_DIR}/bin/conda ]]; then
-        cd env && \
-        wget --no-verbose --continue https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-        chmod a+x Miniconda3-latest-Linux-x86_64.sh
-        (
-            export HOME=$CONDA_DIR
-            ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_DIR -b -f || exit 1
-        )
-
+        cd env
+        curl -fsSL --no-verbose --continue https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > conda-install.sh
+        chmod a+x conda-install.sh
+        HOME=$CONDA_DIR ./conda-install.sh -p $CONDA_DIR -b -f || exit 1
         conda config --system --add envs_dirs $CONDA_DIR/envs
         conda config --system --add pkgs_dirs $CONDA_DIR/pkgs
     fi
